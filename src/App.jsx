5f6cec6a73db8559ff276460ff4c2d69153dd094
Hussam Salah <hussamsalah399@gmail.com>
2024-11-29 00:14:59 +0200

    fix navbar & update cv

--- src/App.jsx
+++ src/App.jsx
@@ -19,22 +19,22 @@ import gsap from 'gsap'
 
 function App() {
 
-// Initialize a new Lenis instance for smooth scrolling
+
 const lenis = new Lenis({
   duration: 1.2,
   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
 });
 
-// Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+
 lenis.on('scroll', ScrollTrigger.update);
 
-// Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
-// This ensures Lenis's smooth scroll animation updates on each GSAP tick
+
+
 gsap.ticker.add((time) => {
-  lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+  lenis.raf(time * 1000); 
 });
 
-// Disable lag smoothing in GSAP to prevent any delay in scroll animations
+
 gsap.ticker.lagSmoothing(0);
 
 useLayoutEffect(() => {
@@ -49,16 +49,16 @@ useLayoutEffect(() => {
 
 
 
-  // useEffect(() => {
+  
 
-  //   const titles = document.querySelectorAll('h2.title');
+  
 
-  //   titles.forEach(heading => {
-  //     const headingText = heading.textContent;
-  //     heading.setAttribute('data-content', headingText);
-  //   });
+  
+  
+  
+  
 
-  // }, [])
+  
 
   return (
     <>
@@ -80,11 +80,11 @@ useLayoutEffect(() => {
       <div>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur culpa aliquam eligendi tenetur minima cum quam perferendis vel nesciunt aperiam facilis excepturi at quasi fugit obcaecati impedit harum dolores, beatae enim alias libero praesentium magnam aliquid quas? Velit autem minima, a similique provident deserunt exercitationem eligendi facere tempore hic quas! Magnam enim quam in nihil impedit, dolorum cupiditate unde magni? Quis at non dignissimos, esse commodi doloremque nihil? Deleniti voluptatibus in laborum, repudiandae ab perspiciatis quisquam magnam. Voluptates, soluta. Rem, fuga quos nulla minima quo ab. Numquam quos perferendis omnis repellendus sint voluptate consequatur ipsam tempora itaque fugit doloremque incidunt, natus, vero quidem rerum voluptatibus quia. Voluptatibus ipsa mollitia, quidem eveniet ipsam hic aliquid alias at. Itaque nihil sunt ea harum ullam voluptatem dignissimos ut accusamus totam delectus quasi doloribus illum libero, quia veritatis. Deleniti minima enim recusandae aperiam cumque, fugiat laborum ipsa quidem natus non. Error, soluta at voluptatem voluptas quibusdam recusandae ut porro veritatis pariatur, praesentium adipisci. Beatae totam laboriosam quam quisquam neque ipsa nemo voluptatum dolores molestias tenetur nobis cupiditate praesentium atque labore, cum aliquam magnam earum non provident adipisci facilis possimus. Velit, molestias. Repudiandae, distinctio necessitatibus saepe dolore cum quidem recusandae at expedita molestiae laborum corrupti reiciendis sequi quas asperiores aspernatur, ducimus quia odit animi rerum delectus. Cum, quibusdam. Iure reiciendis voluptate, aspernatur vero excepturi veritatis cupiditate, eos dolore cum similique ipsam error! Eos numquam perferendis suscipit iusto voluptates magnam mollitia voluptate qui odit ratione! Quisquam inventore praesentium similique placeat delectus earum odio vitae qui! Pariatur placeat blanditiis eius modi eaque! Harum nam atque, similique ratione explicabo beatae earum ipsum magni debitis quam impedit error blanditiis! Nostrum, cum nemo. Ullam molestias at ipsa maiores ea, autem a magnam, veniam amet obcaecati sequi dolores commodi adipisci quis, delectus accusamus quisquam. Enim qui pariatur molestiae vero beatae error alias eos! Itaque, quas? Ad, culpa consectetur eveniet nihil placeat quam rem accusamus nemo, eum, voluptates consequatur incidunt fugiat! Fugit illo debitis vel, eum nisi nulla dolore aspernatur dignissimos recusandae saepe temporibus voluptas explicabo nesciunt sit tempora adipisci ipsa dolor quis porro facere minima accusamus libero molestias? Cumque quidem nam vero sed culpa nesciunt temporibus magni maxime, ad odio commodi expedita amet similique eius. Dignissimos est voluptatibus pariatur accusantium veniam libero quasi expedita illo nemo voluptate modi error a, quam sapiente, voluptatum porro vel? Eum totam adipisci nostrum iusto ipsam minus voluptatum similique in? Laudantium eos quidem doloremque voluptatibus ipsa dolorum mollitia, nesciunt amet id eligendi? Amet sequi quam recusandae. Debitis provident dicta soluta eveniet voluptate pariatur in aspernatur et qui necessitatibus fugiat consectetur amet ab nulla, adipisci hic iste totam autem doloremque! Non quia assumenda reiciendis veniam harum necessitatibus eos cumque impedit quasi ea nam esse eligendi blanditiis libero, accusantium temporibus repudiandae perferendis, iure ipsam molestias mollitia tempore et illo distinctio. Cumque quis neque earum id sapiente quasi debitis in nihil similique. Enim, beatae totam ipsam, provident porro omnis, minus obcaecati voluptatum explicabo accusamus illo! Id, voluptate? Illum ea quibusdam explicabo assumenda totam nesciunt provident officia! Quam, dolorem aliquid?
       </div>
-      <img width='100%' src="https://images.unsplash.com/photo-1725961476494-efa87ae3106a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      <img width='100%' src="https:
       <div>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur culpa aliquam eligendi tenetur minima cum quam perferendis vel nesciunt aperiam facilis excepturi at quasi fugit obcaecati impedit harum dolores, beatae enim alias libero praesentium magnam aliquid quas? Velit autem minima, a similique provident deserunt exercitationem eligendi facere tempore hic quas! Magnam enim quam in nihil impedit, dolorum cupiditate unde magni? Quis at non dignissimos, esse commodi doloremque nihil? Deleniti voluptatibus in laborum, repudiandae ab perspiciatis quisquam magnam. Voluptates, soluta. Rem, fuga quos nulla minima quo ab. Numquam quos perferendis omnis repellendus sint voluptate consequatur ipsam tempora itaque fugit doloremque incidunt, natus, vero quidem rerum voluptatibus quia. Voluptatibus ipsa mollitia, quidem eveniet ipsam hic aliquid alias at. Itaque nihil sunt ea harum ullam voluptatem dignissimos ut accusamus totam delectus quasi doloribus illum libero, quia veritatis. Deleniti minima enim recusandae aperiam cumque, fugiat laborum ipsa quidem natus non. Error, soluta at voluptatem voluptas quibusdam recusandae ut porro veritatis pariatur, praesentium adipisci. Beatae totam laboriosam quam quisquam neque ipsa nemo voluptatum dolores molestias tenetur nobis cupiditate praesentium atque labore, cum aliquam magnam earum non provident adipisci facilis possimus. Velit, molestias. Repudiandae, distinctio necessitatibus saepe dolore cum quidem recusandae at expedita molestiae laborum corrupti reiciendis sequi quas asperiores aspernatur, ducimus quia odit animi rerum delectus. Cum, quibusdam. Iure reiciendis voluptate, aspernatur vero excepturi veritatis cupiditate, eos dolore cum similique ipsam error! Eos numquam perferendis suscipit iusto voluptates magnam mollitia voluptate qui odit ratione! Quisquam inventore praesentium similique placeat delectus earum odio vitae qui! Pariatur placeat blanditiis eius modi eaque! Harum nam atque, similique ratione explicabo beatae earum ipsum magni debitis quam impedit error blanditiis! Nostrum, cum nemo. Ullam molestias at ipsa maiores ea, autem a magnam, veniam amet obcaecati sequi dolores commodi adipisci quis, delectus accusamus quisquam. Enim qui pariatur molestiae vero beatae error alias eos! Itaque, quas? Ad, culpa consectetur eveniet nihil placeat quam rem accusamus nemo, eum, voluptates consequatur incidunt fugiat! Fugit illo debitis vel, eum nisi nulla dolore aspernatur dignissimos recusandae saepe temporibus voluptas explicabo nesciunt sit tempora adipisci ipsa dolor quis porro facere minima accusamus libero molestias? Cumque quidem nam vero sed culpa nesciunt temporibus magni maxime, ad odio commodi expedita amet similique eius. Dignissimos est voluptatibus pariatur accusantium veniam libero quasi expedita illo nemo voluptate modi error a, quam sapiente, voluptatum porro vel? Eum totam adipisci nostrum iusto ipsam minus voluptatum similique in? Laudantium eos quidem doloremque voluptatibus ipsa dolorum mollitia, nesciunt amet id eligendi? Amet sequi quam recusandae. Debitis provident dicta soluta eveniet voluptate pariatur in aspernatur et qui necessitatibus fugiat consectetur amet ab nulla, adipisci hic iste totam autem doloremque! Non quia assumenda reiciendis veniam harum necessitatibus eos cumque impedit quasi ea nam esse eligendi blanditiis libero, accusantium temporibus repudiandae perferendis, iure ipsam molestias mollitia tempore et illo distinctio. Cumque quis neque earum id sapiente quasi debitis in nihil similique. Enim, beatae totam ipsam, provident porro omnis, minus obcaecati voluptatum explicabo accusamus illo! Id, voluptate? Illum ea quibusdam explicabo assumenda totam nesciunt provident officia! Quam, dolorem aliquid?
       </div>
-      <img width='100%' src="https://images.unsplash.com/photo-1725988208207-a15e5f5eb5fb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      <img width='100%' src="https:
       </div> */}
 
     </>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,62 +1,91 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Navbar.module.css';
-import gsap from 'gsap';
-
-import logo from '../../assets/images/h.svg';
 
 import useIsMobileWidth from '../../hooks/useIsMobileWidth';
 
-
 function Navbar() {
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const isMobileWidth = useIsMobileWidth();
 
-
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+    setIsMenuOpen((prev) => !prev);
   };
 
-  // const containerRef = useRef();
+  useEffect(() => {
+    const links = document.querySelectorAll('nav ul li a');
 
-  // useEffect(() => {
-  //   const tl = gsap.timeline();
-  //   tl.from(containerRef.current, { y: 100, duration: 1 });
-  // }, []);
+    // Loop through each link
+    links.forEach((link) => {
+      link.addEventListener('click', handleLinkClick);
+    });
+
+    // Cleanup to remove event listeners
+    return () => {
+      links.forEach((link) => {
+        link.removeEventListener('click', handleLinkClick);
+      });
+    };
+  }, []);
+
+  const handleLinkClick = () => {
+    // Close the menu after navigating
+    setTimeout(() => {
+      setIsMenuOpen(false);
+    }, 0);
+  };
 
   return (
     <>
       <div onClick={toggleMenu} className={styles.menu_btn}>
-        {!isMenuOpen ? <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="20" height="20" fill="currentColor"><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" /></svg> : <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="20" height="20" fill="currentColor"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" /></svg>}
+        {!isMenuOpen ? (
+          <svg
+            className={styles.icon}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            width="20"
+            height="20"
+            fill="currentColor"
+          >
+            <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
+          </svg>
+        ) : (
+          <svg
+            className={styles.icon}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 512"
+            width="20"
+            height="20"
+            fill="currentColor"
+          >
+            <path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" />
+          </svg>
+        )}
       </div>
 
-      <nav className={`${styles.Navbar} ${ isMobileWidth ? isMenuOpen ? 'inset-0': '':'' }`}>
-
-        {/* <img src={logo} width={50} alt="logo"/> */}
-
-
-
-        <ul className={` ${isMenuOpen ? styles.open : ''}`}>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#skills">Skills</a></li>
-          <li><a href="#projects">Projects</a></li>
+      <nav
+        className={`${styles.Navbar} ${isMobileWidth ? (isMenuOpen ? 'inset-0' : '') : ''}`}
+      >
+        <ul className={`${isMenuOpen ? styles.open : ''}`}>
           <li>
-            <a href='#contact' className={`${styles.active} flex items-center`}>
-              <span className="icon inline-block [&_svg]:w-3 mr-2"> {/* Added margin-right for spacing */}
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 9 9">
-                  <path fill="currentColor" fillRule="evenodd" d="M0 0h9v9H8V1.707L.713 8.994l-.707-.707L7.293 1H0z" clipRule="evenodd"></path>
-                </svg>
-              </span>
-              <span>Get in touch</span>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact" className={`${styles.active}`}>
+              Get in touch
             </a>
           </li>
-
         </ul>
-
       </nav>
     </>
-
   );
 }
 
